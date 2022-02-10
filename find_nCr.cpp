d4e87c4c8711e324005ba7ae7e3c94f02a8376e6
Ravindra Kumar <ravindrakar9658@gmail.com>
2022-02-10 12:00:18 +0530

    find ncr

---
+++ find_nCr.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+ fact(int num)
+ {
+     int factorial=1;
+     for(int i=2;i<=num;i++)
+     {
+         factorial*=i;
+
+
+     }
+     return factorial;
+ }
+
+ int main()
+ {
+ int n,r;
+ cout<<" to find ncr enter n and r"<<endl;
+ cin>>n>>r;
+ int ans = fact(n)/(fact(r)*fact(n-r)); // ncr= n!/r!*(n-r)!
+ cout<<ans;
+ return 0;
+
+
+
+ }
