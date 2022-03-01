9f134916b768a1d8ef14a021ab26c6f6bbe02e74
Ravindra Kumar <ravindrakar9658@gmail.com>
2022-03-01 19:44:38 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string>
+#include<iostream>
+using namespace std;
+ int main(){
+     int n;
+     cout<<"how many no  want to sort using binary search"<<endl;
+     cin>>n;
+     int arr[n];
+     for(int i=0; i<;i++)
+{
+    cin>>arr[i];
+
+}
+ int key ;
+ cout<<"enter the no which u want to search  "<<endl;
+ cin>>key;
+
+
+ }
